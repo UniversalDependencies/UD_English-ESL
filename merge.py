c82b57347c838b533fdabd798691ee10b20a91f5
Yevgeni <berzak@mit.edu>
2018-10-11 11:57:07 -0400

    update release version in merge.py

--- merge.py
+++ merge.py
@@ -1,16 +1,16 @@
 import os
 import urllib
 
-UD_RELEASE = "1.4"
+UD_RELEASE = "2.3"
 
 def check_fce():
     code = 0
     try:
         code = os.stat("fce-released-dataset/license").st_size
     except OSError:
-        print '''Error:  FCE folder 'fce-released-dataset' not in the current directory.
+        print('''Error:  FCE folder 'fce-released-dataset' not in the current directory.
         Please download the dataset from https://www.ilexir.co.uk/datasets/index.html
-        and unzip the file in the current directory.'''
+        and unzip the file in the current directory.''')
     return code
 
 def obtain_data(code):
@@ -18,9 +18,9 @@ def obtain_data(code):
     try:
         opener.retrieve("http://esltreebank.org/data-"+UD_RELEASE+"-"+str(code)+"/data.zip", \
                         "data.zip")
-        print "Done, annotations with data are in data.zip"
+        print("Done, annotations with data are in data.zip")
     except IOError:
-        print "Error: Failed obtaining annotations with data from the specified url"
+        print("Error: Failed obtaining annotations with data from the specified url")
         
 def main():
     code = check_fce()
